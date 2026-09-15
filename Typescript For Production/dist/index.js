2e2ae8347af51dc1358f2457ef1383ae630aff05
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-09-15 10:39:41 +0530

    Interface with class example

--- Typescript For Production/dist/index.js
+++ Typescript For Production/dist/index.js
@@ -1,9 +1,48 @@
 "use strict";
+// class User {
+//     public email: string;
+//     public name: string;
+//     private readonly city: string;
+//     constructor(email: string, name: string, city:string) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
 class User {
     constructor(email, name, city) {
         this.email = email;
         this.name = name;
         this.city = city;
+        this._courseCount = 1;
+        this.email = email;
+        this.name = name;
+        this.city = city;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    // in typescript the getters and setters have the keyword get and set unlike java
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNumber;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const rehnoor = new User("rehnoor@gmail.com", "Rehnoor Aulakh", "Patiala");
--- Typescript For Production/src/index.ts
+++ Typescript For Production/src/index.ts
@@ -1,11 +1,51 @@
+// class User {
+//     public email: string;
+//     public name: string;
+//     private readonly city: string;
+//     constructor(email: string, name: string, city:string) {
+//         this.email = email;
+//         this.name = name;
+//         this.city = city;
+//     }
+// }
+
 class User {
-    email: string;
-    name: string;
-    readonly city: string;
-    constructor(email: string, name: string, city:string) {
+
+    protected _courseCount = 1;
+
+    constructor(public email: string, public name: string, private readonly city: string) {
         this.email = email;
         this.name = name;
         this.city = city;
+    }
+
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    // in typescript the getters and setters have the keyword get and set unlike java
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`;
+    }
+
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+    set courseCount(courseNumber: number) {
+        if(courseNumber <= 1) {
+            throw new Error("Course count should be more than 1");
+        } 
+        this._courseCount= courseNumber;
+    } 
+}
+
+class SubUser extends User  {
+    isFamily: boolean = true;
+    
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 
